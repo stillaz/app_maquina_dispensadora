087eb78dee69d4200a23876d2cc15fe76fc141a0
Alexander Aguirre <aguirrealexorri@gmail.com>
2020-03-27 11:09:03 -0500

    Quitar carpeta productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
-    path: 'productos',
-    loadChildren: () => import('./pages/productos/productos.module').then( m => m.ProductosPageModule)
-  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 
 ];
 
